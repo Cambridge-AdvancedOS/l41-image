b69851cace2a6690f91520ee90799ce271f8170f
Ruslan Bukin <br@bsdpad.com>
2020-12-28 16:13:01 +0000

    Add scripts

---
+++ image/mk_sd.sh
@@ -0,0 +1,36 @@
+rm -f sdcard.img.gz sdcard.img
+truncate -s 2000M sdcard.img
+mdconfig -u0 sdcard.img || exit 1
+
+gpart create -s MBR md0
+gpart add -s 128m -t '!4' md0		# msdos (firmware)
+gpart add -s 1200M -t freebsd md0	# /
+gpart add -t freebsd md0		# BSD labels
+gpart set -a active -i 1 md0
+
+gpart create -s BSD md0s3
+gpart add -s 200m -t freebsd-ufs md0s3	# /data
+gpart add -t freebsd-ufs md0s3		# /benchmarks
+
+newfs -j /dev/md0s3a			# /data
+newfs -j /dev/md0s3b			# /benchmarks
+
+# guest home directory
+mount /dev/md0s3a /mnt
+mkdir /mnt/.ssh
+chmod 0755 /mnt
+chmod 0700 /mnt/.ssh
+chown -R 1001:31 /mnt			# guest:guest
+umount /mnt
+
+# Provide boot command for u-boot
+newfs_msdos /dev/md0s1
+mount_msdosfs /dev/md0s1 /mnt
+cp -R ../firmware-rpi4-8g/* /mnt/
+umount /mnt
+
+mdconfig -d -u0
+gzip sdcard.img
+
+#dd if=ubldr of=sdcard.img bs=512 seek=49170 conv=notrunc
+#dd if=rootfs.img of=sdcard.img bs=512 seek=114675 conv=notrunc
