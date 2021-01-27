4966b2fc4eedbb000c1acb5df23c9e4b4223bd44
Ruslan Bukin <br@bsdpad.com>
2021-01-27 19:06:39 +0000

    add a couple experimental scripts

---
+++ image/packages-add.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python2.7
+from commands import getstatusoutput
+import os
+import sys
+
+packages_file = sys.argv[1]
+ROOTFS = sys.argv[2]
+
+PACKAGES = []
+f = open(packages_file, "r")
+for line in f:
+	l = line.strip().split(": ")
+	p = l[0]
+	print(p)
+	PACKAGES.append(p)
+
+for pkg in PACKAGES:
+	print 'Adding package %s' % pkg
+	l = 'pkg -o INSTALL_AS_USER=1 -o METALOG=plop -r %s add -yM %s' % \
+		(ROOTFS, pkg)
+	status, output = getstatusoutput(l)
+	if status != 0:
+		print(output)
+		sys.exit(3)
