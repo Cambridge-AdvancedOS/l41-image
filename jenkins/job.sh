c33f3e935663f50232dc75e7e61b095be7b00a17
Ruslan Bukin <br@bsdpad.com>
2021-01-31 21:46:46 +0000

    add our current script

--- jenkins/job.sh
+++ jenkins/job.sh
@@ -6,23 +6,24 @@ export MAKEOBJDIRPREFIX=$WORKSPACE/obj/
 export HEAD=$WORKSPACE/freebsd
 export KERNCONF=GENERIC-MMCCAM
 export NCPU=`sysctl -n hw.ncpu`
+#export ASSUME_ALWAYS_YES=yes
 
 rm -rf tmp *.img *img.gz
 rm -rf rootfs
-#rm -rf obj
 mkdir -p obj rootfs
 
 #
 # Local distfiles
 #
+#rm -rf $WORKSPACE/distfiles
 mkdir -p $WORKSPACE/distfiles
 cp $WORKSPACE/l41-image/image/distfiles/* $WORKSPACE/distfiles/
 
 #
 # Create packages manifests
 #
-python2.7 $WORKSPACE/l41-image/image/packages.py $WORKSPACE/l41-image/image/packages-node node.mtree || exit $?
-python2.7 $WORKSPACE/l41-image/image/packages.py $WORKSPACE/l41-image/image/packages-mgmt mgmt.mtree || exit $?
+python2.7 $WORKSPACE/l41-image/image/packages.py $WORKSPACE/l41-image/image/packages-node node.mtree $WORKSPACE/distfiles || exit $?
+python2.7 $WORKSPACE/l41-image/image/packages.py $WORKSPACE/l41-image/image/packages-mgmt mgmt.mtree $WORKSPACE/distfiles || exit $?
 
 #
 # Create kernel source manifest
@@ -39,10 +40,12 @@ git --git-dir $WORKSPACE/freebsd/.git rev-parse HEAD > $WORKSPACE/l41-image/imag
 #
 #cd $HEAD && \
 #cat $WORKSPACE/l41-image/image/patches/* | patch -p1 || exit $?
+cp $WORKSPACE/l41-image/rescue/GENERIC-MMCCAM-MDROOT $HEAD/sys/arm64/conf/
 
 #
 # Build FreeBSD
 #
+#rm -rf obj
 #cd $HEAD && \
 #make -j${NCPU} kernel-toolchain && \
 #make -j${NCPU} buildkernel && \
@@ -52,6 +55,11 @@ git --git-dir $WORKSPACE/freebsd/.git rev-parse HEAD > $WORKSPACE/l41-image/imag
 # Build kernel only
 #
 #cd $HEAD && make -j${NCPU} buildkernel || exit $?
+
+#
+# Build rescue kernel
+#
+cd $HEAD && make -j${NCPU} KERNCONF=GENERIC-MMCCAM-MDROOT buildkernel || exit $?
 
 #
 # Install FreeBSD
@@ -67,12 +75,24 @@ make -DNO_ROOT -DWITHOUT_TESTS DESTDIR=$WORKSPACE/rootfs installkernel || exit $
 truncate -s 8G $WORKSPACE/l41-image/image/extras/usr/swap0
 
 #
-# Rootfs node image. 14GB, including 8GB swap
+# Create rescue mdroot fs. 32mb
 #
 cd $WORKSPACE && sh $WORKSPACE/l41-image/image/makeroot.sh \
   -p $WORKSPACE/l41-image/image/extras/etc/master.passwd \
   -g $WORKSPACE/l41-image/image/extras/etc/group \
-  -s 15032385536 -F 10000 \
+  -s 33554432 \
+  -e $WORKSPACE/l41-image/image/extras/extras.mtree \
+  -e $WORKSPACE/l41-image/image/extras-node/extras.mtree \
+  -f $WORKSPACE/l41-image/rescue/basic.files \
+  -d $WORKSPACE/rootfs-rescue.img $WORKSPACE/rootfs/ || exit $?
+
+#
+# Rootfs node image. 4700M
+#
+cd $WORKSPACE && sh $WORKSPACE/l41-image/image/makeroot.sh \
+  -p $WORKSPACE/l41-image/image/extras/etc/master.passwd \
+  -g $WORKSPACE/l41-image/image/extras/etc/group \
+  -s 4928307200 -F 10000 \
   -e $WORKSPACE/l41-image/image/extras/extras.mtree \
   -e $WORKSPACE/l41-image/image/extras-node/extras.mtree \
   -e $WORKSPACE/tmp/node.mtree \
@@ -80,12 +100,12 @@ cd $WORKSPACE && sh $WORKSPACE/l41-image/image/makeroot.sh \
   -d $WORKSPACE/rootfs-node.img $WORKSPACE/rootfs/ || exit $?
 
 #
-# Rootfs mgmt1 image. 14GB, including 8GB swap
+# Rootfs mgmt1 image. 4700M
 #
 cd $WORKSPACE && sh $WORKSPACE/l41-image/image/makeroot.sh \
   -p $WORKSPACE/l41-image/image/extras/etc/master.passwd \
   -g $WORKSPACE/l41-image/image/extras/etc/group \
-  -s 15032385536 -F 10000 \
+  -s 4928307200 -F 10000 \
   -e $WORKSPACE/l41-image/image/extras/extras.mtree \
   -e $WORKSPACE/l41-image/image/extras-mgmt/extras.mtree \
   -e $WORKSPACE/l41-image/image/extras-mgmt1/extras.mtree \
@@ -94,18 +114,38 @@ cd $WORKSPACE && sh $WORKSPACE/l41-image/image/makeroot.sh \
   -d $WORKSPACE/rootfs-mgmt1.img $WORKSPACE/rootfs/ || exit $?
 
 #
-# Rootfs mgmt2 image. 14GB, including 8GB swap
+# Rootfs mgmt2 image. 4700M
 #
 cd $WORKSPACE && sh $WORKSPACE/l41-image/image/makeroot.sh \
   -p $WORKSPACE/l41-image/image/extras/etc/master.passwd \
   -g $WORKSPACE/l41-image/image/extras/etc/group \
-  -s 15032385536 -F 10000 \
+  -s 4928307200 -F 10000 \
   -e $WORKSPACE/l41-image/image/extras/extras.mtree \
   -e $WORKSPACE/l41-image/image/extras-mgmt/extras.mtree \
   -e $WORKSPACE/l41-image/image/extras-mgmt2/extras.mtree \
   -e $WORKSPACE/tmp/mgmt.mtree \
   -e $HEAD/sys/files.mtree \
   -d $WORKSPACE/rootfs-mgmt2.img $WORKSPACE/rootfs/ || exit $?
+
+#
+# Rootfs mgmt3 image. 4700M
+#
+cd $WORKSPACE && sh $WORKSPACE/l41-image/image/makeroot.sh \
+  -p $WORKSPACE/l41-image/image/extras/etc/master.passwd \
+  -g $WORKSPACE/l41-image/image/extras/etc/group \
+  -s 4928307200 -F 10000 \
+  -e $WORKSPACE/l41-image/image/extras/extras.mtree \
+  -e $WORKSPACE/l41-image/image/extras-mgmt/extras.mtree \
+  -e $WORKSPACE/l41-image/image/extras-mgmt3/extras.mtree \
+  -e $WORKSPACE/tmp/mgmt.mtree \
+  -e $HEAD/sys/files.mtree \
+  -d $WORKSPACE/rootfs-mgmt3.img $WORKSPACE/rootfs/ || exit $?
+
+#
+# Create rescue kernel
+#
+cp $WORKSPACE/obj/usr/local/jenkins/workspace/l41-rpi4-image/freebsd/arm64.aarch64/sys/GENERIC-MMCCAM-MDROOT/kernel $WORKSPACE/kernel.rescue && \
+sh $HEAD/sys/tools/embed_mfs.sh kernel.rescue $WORKSPACE/rootfs-rescue.img || exit $?
 
 #
 # SD card node image
@@ -135,8 +175,18 @@ dd if=rootfs-mgmt2.img of=sdcard-mgmt2.img bs=512 seek=264280 conv=notrunc && \
 gzip sdcard-mgmt2.img || exit $?
 
 #
+# SD card mgmt3 image
+#
+cd $WORKSPACE && \
+cp $WORKSPACE/l41-image/sdcard-mgmt/sdcard.img.gz $WORKSPACE/sdcard-mgmt3.img.gz && \
+gunzip sdcard-mgmt3.img.gz && \
+dd if=rootfs-mgmt3.img of=sdcard-mgmt3.img bs=512 seek=264280 conv=notrunc && \
+gzip sdcard-mgmt3.img || exit $?
+
+#
 # Optional artifact
 #
 #gzip $WORKSPACE/rootfs-node.img
 #gzip $WORKSPACE/rootfs-mgmt1.img
 #gzip $WORKSPACE/rootfs-mgmt2.img
+#gzip $WORKSPACE/rootfs-mgmt3.img
