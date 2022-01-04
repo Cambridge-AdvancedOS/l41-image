48227c8b9c8d5865d0774556f63c131b67773b7b
Ruslan Bukin <br@bsdpad.com>
2022-01-04 16:18:53 +0000

    Try to replace standard growfs script with our own, so we can not just resize disks and growfs but also add the swap partition. This has to be tested.

---
+++ image/extras/usr/local/bin/l41_growfs.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+FILENAME=/etc/l41_growfs_completed
+
+if test -f "$FILENAME"; then
+	exit 0
+fi
+
+#
+# Resize the second MBR partition, which is BSD labels partition
+#
+/sbin/gpart resize -i 2 sdda0 || exit 1
+
+#
+# Resize the first BSD label, which holds FreeBSD rootfs
+#
+/sbin/gpart resize -i 1 -s 55G sdda0s2 || exit 2
+
+#
+# Add the swap BSD label
+#
+/sbin/gpart add -t freebsd-swap sdda0s2 || exit 3
+
+#
+# Now grow rootfs BSD label
+#
+/sbin/growfs -y /dev/sdda0s2a || exit 4
+
+#
+# Activate swap BSD label
+#
+/sbin/swapon -a
+
+touch $FILENAME
